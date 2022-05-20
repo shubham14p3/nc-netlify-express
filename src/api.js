6f789d615a757f2ef5bf3842f1ff93cf19d10f40
Shubham Raj <shubham14p3@gmail.com>
2022-05-20 16:40:13 +0530

    Adding Route file

--- src/api.js
+++ src/api.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const dotenv = require("dotenv");
+var cors = require("cors");
 
 const app = express();
 const router = express.Router();
+app.use(cors());
+dotenv.config({
+  path: "D:/Company Work/Nise-Comport/ns-db-men/server" + "/config.env",
+}); 
+// router.get("/", (req, res) => {
+//   res.json({
+//     hello: "hi!"
+//   });
+// });
+app.use(express.json());
+const mongodb = require("./db/conn");
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
-});
+mongodb(); // Connecting DB
 
-app.use(`/.netlify/functions/api`, router);
+const routerLocal="./router/auth";
+app.use(`"/.netlify/functions"+${routerLocal}`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
