0bc1258e0d90db725d53526fc45c6ae7e6c332bc
Shubham Raj <shubham14p3@gmail.com>
2022-05-21 13:12:38 +0530

    Working code

--- src/api.js
+++ src/api.js
@@ -1,26 +1,16 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const dotenv = require("dotenv");
-var cors = require("cors");
 
 const app = express();
 const router = express.Router();
-app.use(cors());
-dotenv.config({
-  path: "D:/Company Work/Nise-Comport/ns-db-men/server" + "/config.env",
-}); 
-// router.get("/", (req, res) => {
-//   res.json({
-//     hello: "hi!"
-//   });
-// });
-app.use(express.json());
-const mongodb = require("./db/conn");
 
-mongodb(); // Connecting DB
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
 
-const routerLocal="./router/auth";
-app.use(`"/.netlify/functions"+${routerLocal}`, router);
+app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
